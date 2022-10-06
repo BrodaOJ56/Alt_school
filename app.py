ef921244303baa168baeb7d8cbdacc6410c7b151
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2022-10-06 13:34:08 +0100

    updates

--- app.py
+++ app.py
@@ -91,9 +91,10 @@ def delete_user(id):
     return redirect(url_for('index'))
 
     
-port = int(os.environ.get('PORT', 5000)) 
+ 
 
 if __name__ == "__main__":
     db.create_all()
     
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
